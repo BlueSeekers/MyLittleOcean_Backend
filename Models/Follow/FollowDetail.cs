7b15655eb96899d181d9fad76afa037f8b3449e7
김수진 <nuu042@naver.com>
2025-01-10 15:25:30 +0900

    [Update] FollowDto Swagger Annotation 추가

--- Models/Follow/FollowDetail.cs
+++ Models/Follow/FollowDetail.cs
@@ -1,7 +1,10 @@
-﻿namespace MyLittleOcean.Models.Follow;
+﻿using Swashbuckle.AspNetCore.Annotations;
 
 public class FollowDetail {
+
+    [SwaggerSchema("유저 식별번호")]
     public int userNo {  get; set; }
 
+    [SwaggerSchema("유저 이름")]
     public string? userName { get; set; }
 }
